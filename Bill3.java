97cc889e18d17b237fd03073f1d027b9895a26a8
Omri Keynan <omrikeynan@Omris-MacBook-Pro-2.local>
2024-11-08 22:31:49 +0200

    Wrote a program that splits a bill evenly among three diners

--- Bill3.java
+++ Bill3.java
@@ -5,9 +5,9 @@ public class Bill3 {
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-		int bill = Integer.parseInt(args[3])/3;
+		int bill = Integer.parseInt(args[3]);
 	   
-		System.out.println("Dear" + " " + name1 + "," + " " + name2 + "," + " " + "and" + 
-							" " + name3 + ":" + " " + "pay" + " " + Math.ceil(bill) + " " + "Shekels each");
+		System.out.println("Dear " + name3 + ", " + name2 + ", " + "and " + 
+							name1 + ": " + "pay " + Math.ceil((double) bill/3) + " Shekels each.");
 	}
 }
