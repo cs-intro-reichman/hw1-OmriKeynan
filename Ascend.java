e5a3c7d7867ef188c5e461e3595c2c6921176462
Omri Keynan <omri.keynan@post.runi.ac.il>
2024-11-13 00:34:32 +0200

    Completed Assignment

--- Ascend.java
+++ Ascend.java
@@ -2,7 +2,6 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		
 		int lim = Integer.parseInt(args[0]);
 		int a = ((int)(Math.random() * lim));
 		int b = ((int)(Math.random() * lim));
